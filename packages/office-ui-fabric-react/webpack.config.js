505f865716ba1703b8c032ebe71192948cc2760f
Erich Abellera <a.erich@gmail.com>
2017-09-07 13:45:11 -0700

    Update config to bundle demo-app and copy to dist

--- packages/office-ui-fabric-react/webpack.config.js
+++ packages/office-ui-fabric-react/webpack.config.js
@@ -4,17 +4,33 @@ const resources = require('../../scripts/tasks/webpack-resources');
 const BUNDLE_NAME = 'office-ui-fabric-react';
 const IS_PRODUCTION = process.argv.indexOf('--production') > -1;
 
+let entry = {
+  [BUNDLE_NAME]: './lib/index.js'
+};
+
+// In production builds, produce the demo-app bundle.
+if (IS_PRODUCTION) {
+  entry['demo-app'] = './lib/demo/index.js';
+}
+
 module.exports = resources.createConfig(
   BUNDLE_NAME,
   IS_PRODUCTION,
   {
-    entry: {
-      [BUNDLE_NAME]: './lib/index.js'
-    },
+    entry,
 
     output: {
       libraryTarget: 'var',
       library: 'Fabric'
+    },
+
+    resolve: {
+      alias: {
+        'office-ui-fabric-react/src': path.join(__dirname, 'src'),
+        'office-ui-fabric-react/lib': path.join(__dirname, 'lib'),
+        'Props.ts.js': 'Props',
+        'Example.tsx.js': 'Example'
+      }
     },
 
     externals: [
