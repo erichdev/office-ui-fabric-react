5c0efc19d8732c425df2c87c25ce0ac1707396a2
Erich Abellera <a.erich@gmail.com>
2017-09-08 11:03:26 -0700

    Update deploy script to use argv, change uploaded file

--- scripts/deploy-demo.js
+++ scripts/deploy-demo.js
@@ -5,7 +5,7 @@ let argv = require('yargs').argv;
 let config = {
   host: argv.host,
   user: argv.user,
-  pass: argv.password
+  pass: process.argv[3]
 };
 
 console.log('HOST', config.host);
@@ -16,60 +16,12 @@ let Ftp = new JSFtp(config);
 
 Ftp.keepAlive(2000);
 
-// //
-// Ftp.list('site/wwwroot', function (err, res) {
-//   console.log('inside list');
 
-//   if (err)
-//     console.error(err);
-
-//   console.log('content: ' + res);
-// });
-
-// Ftp.on('data', function (data) {
-//   console.log('DATA: ', data);
-// })
-
-// Ftp.on('connect', function (data) {
-//   console.log('CONNECT: ', data);
-// })
-// //
-
-// //
-// Ftp.on('progress', function (data) {
-//   console.log('PROGRESS: ', data);
-// })
-
-// Ftp.on('error', function (data) {
-//   console.log('ERROR: ', data);
-// })
-// //
-
-const demo = fs.readFileSync('packages/office-ui-fabric-react/dist/demo-app.js');
-Ftp.put(demo, 'site/wwwroot/v-erabe/demo-app.js', function (err) {
+const demo = fs.readFileSync('packages/office-ui-fabric-react/tslint.json');
+Ftp.put(demo, 'site/wwwroot/v-erabe/tslint.json', function (err) {
   if (!err)
     console.log("File transferred successfully!");
 
   else
     console.error('ERROR UPLOADING: ' + err);
 });
-
-Ftp = new JSFtp(config);
-
-const html = fs.readFileSync('packages/office-ui-fabric-react/dist/index.html');
-Ftp.put(html, 'site/wwwroot/v-erabe/index.html', function (err) {
-  if (!err)
-    console.log("File transferred successfully!");
-
-  else
-    console.error('ERROR UPLOADING: ' + err);
-});
-
-
-// Ftp.put('../packages/office-ui-fabric-react/dist/demo-app.js', 'site/wwwroot/v-erabe/demo-app.js', function (err) {
-//   if (!err)
-//     console.log("File transferred successfully!");
-
-//   else
-//     console.error('Error uploading' + err);
-// });
