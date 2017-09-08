61ec5a41e701b6f86f8c3a628cf3a912ede5c44b
Erich Abellera <a.erich@gmail.com>
2017-09-08 09:58:01 -0700

    Add logging to deploy script for debug

--- scripts/deploy-demo.js
+++ scripts/deploy-demo.js
@@ -8,39 +8,42 @@ let config = {
   pass: argv.password
 };
 
+console.log('HOST', config.host);
+console.log('USER', config.user);
+console.log('pass', config.pass.substring(0, 4));
 
 let Ftp = new JSFtp(config);
 
 Ftp.keepAlive(2000);
 
-// //
-// Ftp.list('site/wwwroot', function (err, res) {
-//   console.log('inside list');
+//
+Ftp.list('site/wwwroot', function (err, res) {
+  console.log('inside list');
 
-//   if (err)
-//     console.error(err);
+  if (err)
+    console.error(err);
 
-//   console.log('content: ' + res);
-// });
+  console.log('content: ' + res);
+});
 
-// Ftp.on('data', function (data) {
-//   console.log('DATA: ', data);
-// })
+Ftp.on('data', function (data) {
+  console.log('DATA: ', data);
+})
 
-// Ftp.on('connect', function (data) {
-//   console.log('CONNECT: ', data);
-// })
-// //
+Ftp.on('connect', function (data) {
+  console.log('CONNECT: ', data);
+})
+//
 
-// //
-// Ftp.on('progress', function (data) {
-//   console.log('PROGRESS: ', data);
-// })
+//
+Ftp.on('progress', function (data) {
+  console.log('PROGRESS: ', data);
+})
 
-// Ftp.on('error', function (data) {
-//   console.log('ERRRR: ', data);
-// })
-// //
+Ftp.on('error', function (data) {
+  console.log('ERRRR: ', data);
+})
+//
 
 const demo = fs.readFileSync('packages/office-ui-fabric-react/dist/demo-app.js');
 Ftp.put(demo, 'site/wwwroot/v-erabe/demo-app.js', function (err) {
